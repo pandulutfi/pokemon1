a2277c0f939c709177440c2793cf47a7fd2b684c
Andre Ruffino <ruffino.andre@gmail.com>
2021-04-01 09:23:54 +0700

    first commit

---
+++ src/PersonList.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  state = {
+    name: '',
+  }
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      name: this.state.name
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const user = {
+      name: this.state.name
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+handleSubmit = event => {
+    event.preventDefault();
+
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${this.state.id}`)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person ID:
+            <input type="text" name="id" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Delete</button>
+        </form>
+      </div>
+    )
+  }
+}
---
+++ src/api.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios from 'axios';
+
+import API from '../api';
+
+export default class PersonList extends React.Component {
+  handleSubmit = event => {
+    event.preventDefault();
+
+    API.delete(`users/${this.state.id}`)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+}
+
+handleSubmit = async event => {
+  event.preventDefault();
+
+  //
+  const response = await API.delete(`users/${this.state.id}`);
+
+  console.log(response);
+  console.log(response.data);
+};
